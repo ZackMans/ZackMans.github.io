063b34a3e7a08c721a6ed018a99b5e4d263ccb1b
MeMansッ <80680923+ZackMans@users.noreply.github.com>
2022-08-05 06:30:16 +0700

    Update decrypt-bookmarklet.js

--- link-lock/hidden/decrypt-bookmarklet.js
+++ link-lock/hidden/decrypt-bookmarklet.js
@@ -97,7 +97,7 @@ try {
   if (params.unencrypted) {
     window.location.href = params.url;
   } else {
-    window.location.href = "https://jstrieb.github.io/link-lock/" + window.location.hash;
+    window.location.href = "https://zackmans.github.io/link-lock/" + window.location.hash;
   }
 } catch {
   window.location.replace("https://gmail.com");
